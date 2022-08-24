59d93d4e21469c85b4402b824329ef75f22bc850
Ajitkumar6 <ajitcareerit@gmail.com>
2022-08-24 16:23:15 +0530

    Functional and class components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to React Learning</h1>
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Namasthe React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>Welcome to React Learning</h1>
+//         {/* <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p> */}
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Namasthe React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// function MyWebsite() {
+//   return (
+//     <h1>Welcome All</h1>
+//   );
+// }
+
+class MyWebsite extends React.Component {
+  render() {
+    return (
+      <h1>Welcome All ! Namasthe !</h1>
+    );
+  }
 }
 
-export default App;
+export default MyWebsite;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import "./index.css";
+import MyWebsite from './App';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// let myStyle = {
+//   color:'blue',
+//   textAlign:'center'
+// } 
+// let myEle = 
+// <div className='myStyle'>
+//   <h1>Namasthe Ji</h1>
+//   <h2>Pado ReactJS</h2>
+//   <h3>Mazaa Karo !!!</h3>
+// </div>
+// ReactDOM.render(myEle,document.getElementById('root'));
+
+// function Header() {
+//   return (
+//     <div>
+//     <h1>Namasthe Baiya !!</h1>
+//     <h2>ReactJS acchaa hai</h2>
+//     </div>    
+//   );
+// }
+// function Sidebar() {
+//   return (
+//     <h1>Sidebar Page</h1>
+//   );
+// }
+// function NavBar() {
+//   return (
+//     <h1>NavBar Page</h1>
+//   );
+// }
+// function MyWebsite() {
+//   return (
+//         <div>
+//           <Header/>
+//           <Sidebar/>
+//           <NavBar/>
+//         </div>
+//   );
+// // }
+// ReactDOM.render(<MyWebsite/>,document.getElementById("root"));
+
+// class MyWebsite extends React.Component {
+//   render() {
+//     return (
+//       <h1>Welcome All</h1>
+//     );
+//   }
+// }
+class MyNewWebsite extends React.Component {
+  render() {
+    return (
+      <div>
+        <MyWebsite/>
+        <h2>JS, React, Redux</h2>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<MyNewWebsite/>,document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
