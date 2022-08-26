17d8a266b223940b0d17d3adedbbb0f81313a907
Ajitkumar6 <ajitcareerit@gmail.com>
2022-08-26 18:52:17 +0530

    React basic concepts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css";
-import MyWebsite from './App';
+// import MyWebsite from './App';
 // import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
@@ -62,18 +62,93 @@ import MyWebsite from './App';
 //     );
 //   }
 // }
-class MyNewWebsite extends React.Component {
-  render() {
-    return (
-      <div>
-        <MyWebsite/>
-        <h2>JS, React, Redux</h2>
-      </div>
-    )
-  }
-}
+// class MyNewWebsite extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <MyWebsite/>
+//         <h2>JS, React, Redux</h2>
+//       </div>
+//     )
+//   }
+// }
 
-ReactDOM.render(<MyNewWebsite/>,document.getElementById("root"));
+// class LearnReact extends React.Component {
+//   render() {
+//     return (
+//       <h1>Learn ReactJS in {this.props.vari} {this.props.method}</h1>
+//     )
+//   }
+// }
+
+// class LearnReact2 extends React.Component {
+//   render() {
+//     const v = "English"
+//     return (
+//       <LearnReact vari={v}/>
+//     )
+//   }
+// }
+
+// LearnReact.defaultProps = {language:"English",method:"easily"};
+// ReactDOM.render(<LearnReact2/>,document.getElementById("root"));
+
+// class LearnReact extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {initialValue:"Namasthe",people:"Guys"}
+//   }
+//   initialValue = () => {
+//     this.setState({initialValue:"Namasthe"})
+//   }
+//   changeValue = () => {
+//     this.setState({initialValue:"See You Again"})
+//   }
+//   render() {
+//     return (
+//       <div>
+//       <h1>{this.state.initialValue} {this.state.people}</h1>
+//       <button type="button" onClick={this.initialValue}>Enter</button>
+//       <button type="button" onClick={this.changeValue}>Exit</button>
+//       </div>
+//     )
+//   }
+// }
+
+// ReactDOM.render(<LearnReact/>,document.getElementById("root"))
+
+// const sampleArray = [1,2,3,4,5,6]
+// function MyList (props) {
+//   const sampleArray = props.sampleArray
+//   const displayList = sampleArray.map((li) => {
+//     return <li>{li}</li>
+//   })
+//   return(
+//     <ul>{displayList}</ul>
+//   )
+// }
+
+// ReactDOM.render(<MyList sampleArray={sampleArray}/>,document.getElementById("root"));
+
+// function MyList(props) {
+//   const key = props.index
+//   const values = props.value
+//   return (
+//     <li> {key} {values}</li>
+//   )
+// }
+function ListKeyExample(props) {
+  const sampleArray = props.myArray
+  const sampleArrayMap = sampleArray.map((value,ind)=>{
+    // return <MyList value={value} index={ind}/>
+    return <li>{ind} {value}</li> 
+  })
+  return(
+    <ul>{sampleArrayMap}</ul>
+    )  
+  }
+  const sampleArray = [1,2,3,4,5,6]
+ReactDOM.render(<ListKeyExample myArray={sampleArray}/>, document.getElementById("root"))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
